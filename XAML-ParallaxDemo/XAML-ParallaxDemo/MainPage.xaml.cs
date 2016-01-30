369d90f4b85f7ad432ac468ca4da01fc4e0127c8
James Clarke <james@clarkezone.net>
2016-01-30 12:42:55 -0800

    1) Add conditional logic to check for Win10 TH2 Update
    2) Cleanup, add comments

--- XAML-ParallaxDemo/XAML-ParallaxDemo/MainPage.xaml.cs
+++ XAML-ParallaxDemo/XAML-ParallaxDemo/MainPage.xaml.cs
@@ -15,17 +15,34 @@ namespace XAML_ParallaxDemo
 
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            CompositionPropertySet scrollerViewerManipulation = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(myScroller);
+            //Only run this code if the app is running on Windows 10 1511 or later because API not supported on earlier versions of Windows
 
-            Compositor compositor = scrollerViewerManipulation.Compositor;
+            if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract",2)) {
 
-            ExpressionAnimation expression = compositor.CreateExpressionAnimation("ScrollManipululation.Translation.Y * ParallaxMultiplier");
+                // Get a referece to a "propertyset" that contains the following keys:
+                //  Translation
+                //  CenterPoint
+                //  Scale
+                //  Matrix
+                // that represent the state of the scrollview at any moment (i.e. as the user manipulates the scrollviewer with mouse, touch, touchpad)
 
-            expression.SetScalarParameter("ParallaxMultiplier", 0.3f);
-            expression.SetReferenceParameter("ScrollManipululation", scrollerViewerManipulation);
+                CompositionPropertySet scrollerViewerManipulation = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(myScroller);
 
-            Visual textVisual = ElementCompositionPreview.GetElementVisual(background);
-            textVisual.StartAnimation("Offset.Y", expression);
+                Compositor compositor = scrollerViewerManipulation.Compositor;
+
+                // Create the expression
+                ExpressionAnimation expression = compositor.CreateExpressionAnimation("ScrollManipululation.Translation.Y * ParallaxMultiplier");
+
+                // wire the ParallaxMultiplier constant into the expression
+                expression.SetScalarParameter("ParallaxMultiplier", 0.3f);
+
+                // set "dynamic" reference parameter that will be used to evaluate the current position of the scrollbar every frame
+                expression.SetReferenceParameter("ScrollManipululation", scrollerViewerManipulation);
+
+                // Get the background image and start animating it's offset using the expression
+                Visual backgroundVisual = ElementCompositionPreview.GetElementVisual(background);
+                backgroundVisual.StartAnimation("Offset.Y", expression);
+            }
         }
     }
 }
