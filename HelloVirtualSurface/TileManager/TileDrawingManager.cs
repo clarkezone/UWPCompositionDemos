7872ace30a954ce76ae6156a60443e7c28576c73
James Clarke (WDG) <jeclarke@ntdev.microsoft.com>
2017-04-03 09:14:57 -0700

    fix drawahead logic

--- HelloVirtualSurface/TileManager/TileDrawingManager.cs
+++ HelloVirtualSurface/TileManager/TileDrawingManager.cs
@@ -7,9 +7,10 @@ namespace TileManager
     public class TileDrawingManager
     {
         private const int TILESIZE = 250;
-        private const int DRAWAHEAD = 0; //Number of tiles to draw ahead //Note: drawahead doesn't currently work
+        private const int DRAWAHEAD = 5; //Up to this many extra tiles will be drawn on each edge outside the viewport
         internal int currentTopLeftTileRow = 0;
         internal int currentTopLeftTileColumn = 0;
+
         internal int drawnTopTileRow = 0;
         internal int drawnBottomTileRow = 0;
         internal int drawnLeftTileColumn = 0;
@@ -26,74 +27,72 @@ namespace TileManager
 
         public string UpdateVisibleRegion(Vector3 currentPosition)
         {
+            this.currentPosition = currentPosition;
             bool stateUpdate = false;
+            
+            int requiredTopTileRow = Math.Max((int)currentPosition.Y / TILESIZE - DRAWAHEAD, 0);
+            int requiredBottomTileRow = (int)(currentPosition.Y + viewPortSize.Height) / TILESIZE + DRAWAHEAD;
+            int requiredLeftTileColumn = Math.Max((int)currentPosition.X / TILESIZE - DRAWAHEAD, 0);
+            int requiredRightTileColumn = (int)(currentPosition.X + viewPortSize.Width) / TILESIZE + DRAWAHEAD;
+
+            currentTopLeftTileRow = (int)currentPosition.Y / TILESIZE;
+            currentTopLeftTileColumn = (int)currentPosition.X / TILESIZE;
+
+            for (int row = requiredTopTileRow; row < drawnTopTileRow; row++)
+            {
+                for (int column = drawnLeftTileColumn; column <= drawnRightTileColumn; column++)
+                {
+                    DrawTile(row, column);
+                    stateUpdate = true;
+                }
+            }
+
+
+            for (int row = drawnBottomTileRow + 1; row <= requiredBottomTileRow; row++)
+            {
+                for (int column = drawnLeftTileColumn; column <= drawnRightTileColumn; column++)
+                {
+                    DrawTile(row, column);
+                    stateUpdate = true;
+                }
+            }
+            drawnTopTileRow = Math.Min(requiredTopTileRow, drawnTopTileRow);
+            drawnBottomTileRow = Math.Max(requiredBottomTileRow, drawnBottomTileRow);
+
+
+            for (int column = requiredLeftTileColumn; column < drawnLeftTileColumn; column++)
+            {
+                for (int row = drawnTopTileRow; row <= drawnBottomTileRow; row++)
+                {
+                    DrawTile(row, column);
+                    stateUpdate = true;
+                }
+            }
+
+
+            for (int column = drawnRightTileColumn + 1; column <= requiredRightTileColumn; column++)
+            {
+                for (int row = drawnTopTileRow; row <= drawnBottomTileRow; row++)
+                {
+                    DrawTile(row, column);
+                    stateUpdate = true;
+                }
+            }
+
+            drawnLeftTileColumn = Math.Min(requiredLeftTileColumn, drawnLeftTileColumn);
+            drawnRightTileColumn = Math.Max(requiredRightTileColumn, drawnRightTileColumn);
+
 
             //TODO: perf optimization to batch draw tile calls into a single drawingsession scope
 
-            if (currentPosition.X > (currentTopLeftTileColumn + 1) * TILESIZE) // scrolling left, need new items to the right
-            {
-                stateUpdate = true;
-                currentTopLeftTileColumn++;
-                this.currentPosition.X += TILESIZE;
-
-                for (int row = currentTopLeftTileRow; row < currentTopLeftTileRow + verticalVisibleTileCount + DRAWAHEAD; row++)
-                {
-                    for (int column = drawnRightTileColumn + 1; column <= drawnRightTileColumn + 1 + DRAWAHEAD; column++)
-                    {
-                        DrawTile(row, column);
-                    }
-                }
-                drawnRightTileColumn++;
-            } else
-            if (currentPosition.X >= 0 && currentPosition.X < (currentTopLeftTileColumn * TILESIZE)) // scrolling right, need new items to the right
-            {
-                stateUpdate = true;
-                currentTopLeftTileColumn--;
-                    this.currentPosition.X -= TILESIZE;
-
-                for (int row = currentTopLeftTileRow; row < currentTopLeftTileRow + verticalVisibleTileCount + DRAWAHEAD; row++)
-                {
-                    for (int column = drawnLeftTileColumn - 1; column >= drawnLeftTileColumn - 1 - DRAWAHEAD; column--)
-                    {
-                        DrawTile(row, column);
-                    }
-                }
-                drawnLeftTileColumn--;
-            } else if (currentPosition.Y > (currentTopLeftTileRow + 1) * TILESIZE) // scrolling down, need new items on bottom
-            {
-                stateUpdate = true;
-                currentTopLeftTileRow++;
-                this.currentPosition.Y += TILESIZE;
-
-                for (int row = drawnBottomTileRow + 1; row <= drawnBottomTileRow + 1 + DRAWAHEAD; row++)
-                {
-                    for (int column = drawnLeftTileColumn; column <= drawnRightTileColumn; column++)
-                    {
-                        DrawTile(row, column);
-                    }
-                }
-                drawnBottomTileRow++;
-            }
-            else if (currentPosition.Y >= 0 && currentPosition.Y < (currentTopLeftTileRow * TILESIZE)) // scrolling up, need new items on top
-            {
-                stateUpdate = true;
-                currentTopLeftTileRow--;
-                this.currentPosition.Y -= TILESIZE;
-
-                for (int row = drawnTopTileRow - 1; row >= drawnTopTileRow - 1 - DRAWAHEAD; row--)
-                {
-                    for (int column = drawnLeftTileColumn; column <= drawnRightTileColumn; column++)
-                    {
-                        DrawTile(row, column);
-                    }
-                }
-                drawnTopTileRow--;
-            }
-
+            //
+            // Consider doing something so that we don't draw tiles above that are simply going to get thrown away 
+            // down here - might be as simple as trimming 
+            //
 
             if (stateUpdate)
             {
-                Trim();
+                Trim(requiredLeftTileColumn, requiredTopTileRow, requiredRightTileColumn, requiredBottomTileRow);
             }
 
             return $"Left tile:{currentTopLeftTileColumn} Top tile:{currentTopLeftTileRow}";
@@ -142,20 +141,20 @@ namespace TileManager
             this.currentRenderer.DrawTile(GetRectForTile(row, column), row, column); //index's are 0 based
         }
 
-        private void Trim()
+        private void Trim(int leftColumn, int topRow, int rightColumn, int bottomRow)
         {
             var trimRect = GetRectForTileRange(
-                currentTopLeftTileColumn,
-                currentTopLeftTileRow,
-                drawnRightTileColumn - drawnLeftTileColumn +1,
-                drawnBottomTileRow - drawnTopTileRow +1);
+                leftColumn,
+                topRow,
+                rightColumn - leftColumn + 1,
+                bottomRow - topRow + 1);
+
             this.currentRenderer.Trim(trimRect);
 
-            drawnLeftTileColumn = currentTopLeftTileColumn;
-            drawnRightTileColumn = currentTopLeftTileColumn + horizontalVisibleTileCount -1;
-
-            drawnTopTileRow = currentTopLeftTileRow;
-            drawnBottomTileRow = currentTopLeftTileRow + verticalVisibleTileCount -1;
+            drawnLeftTileColumn = leftColumn;
+            drawnRightTileColumn = rightColumn;
+            drawnTopTileRow = topRow;
+            drawnBottomTileRow = bottomRow;
         }
 
 
