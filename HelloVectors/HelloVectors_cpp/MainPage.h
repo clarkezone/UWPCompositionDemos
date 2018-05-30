181b5d2c0c34198e6203475c5ac8a3fa24a5196b
James Clarke (OS GROUP) <jeclarke@ntdev.microsoft.com>
2018-05-30 08:14:12 -0700

    add c++ sample

---
+++ HelloVectors/HelloVectors_cpp/MainPage.h
@@ -0,0 +1,39 @@
+﻿//
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::HelloVectors_cpp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+		Windows::UI::Composition::Compositor compositor{ nullptr };
+
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+		void SimpleShapeImperative_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+		void PathMorphImperative_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+		
+		void SimplePathImperative_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+		Microsoft::Graphics::Canvas::Geometry::CanvasGeometry BuildSquareGeometry();
+
+		Microsoft::Graphics::Canvas::Geometry::CanvasGeometry BuildCircleGeometry();
+
+		Windows::UI::Composition::CompositionLinearGradientBrush CreateGradientBrush();
+    };
+}	
+
+namespace winrt::HelloVectors_cpp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
---
+++ HelloVectors/HelloVectors_cpp/pch.h
@@ -0,0 +1,23 @@
+﻿//
+// pch.h
+// Header for platform projection include files
+//
+
+#pragma once
+
+#define NOMINMAX
+
+#include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.ApplicationModel.Activation.h"
+#include "winrt/Windows.UI.Composition.h"
+#include "winrt/Windows.UI.Xaml.h"
+#include "winrt/Windows.UI.Xaml.Controls.h"
+#include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
+#include "winrt/Windows.UI.Xaml.Data.h"
+#include "winrt/Windows.UI.Xaml.Hosting.h"
+#include "winrt/Windows.UI.Xaml.Interop.h"
+#include "winrt/Windows.UI.Xaml.Markup.h"
+#include "winrt/Windows.UI.Xaml.Navigation.h"
+#include "winrt/Windows.UI.Xaml.Shapes.h"
+#include "Generated Files/winrt/Microsoft.Graphics.Canvas.h"
+#include "Generated Files/winrt/Microsoft.Graphics.Canvas.Geometry.h"
